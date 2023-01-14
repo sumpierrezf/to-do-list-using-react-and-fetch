fa368ee5a3492d59367fcef8c08eefa09f62bf7c
sumpierrezf <viqui09umpierrez@gmail.com>
2023-01-14 16:16:45 +0000

    Creando funcionalidades

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,98 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	
+	const [tarea,setTarea] = useState("");
+	const [list,setList] = useState([]);
+
+
+	/*const listItems = list.map((item) =>
+	<li key={item.toString()}>
+	{item}
+  </li>
+	);*/
+
+	const DeleteItems = (indexItem) => {
+		setList((prevState) =>
+		  prevState.filter((listItems, index) => index !== indexItem)
+		);
+	  };
+
+	function enviarDatos(e) {
+		e.preventDefault()
+		setList([...list, tarea]);
+		setTarea("")
+	}
+	console.log(list);
+
+	function crearUsuario(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/svuf`,{method: 'POST', headers: {
+			'Content-Type': 'application/json'},
+			body: JSON.stringify([])
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+	}
+
+	function obtenerLista(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/svuf`,{method: 'GET', headers: {
+			'Content-Type': 'application/json'}
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+	}
+
+	function actualizar(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/svuf`,{method: 'PUT', headers: {
+			'Content-Type': 'application/json'},
+			body: JSON.stringify([list])
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+	}
+
+	function borrarLista(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/svuf`,{method: 'DELETE', headers: {
+			'Content-Type': 'application/json'}
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+	}
+
+	useEffect (()=>{
+		crearUsuario();
+		obtenerLista()
+	},[])
+
+	useEffect (()=>{
+		actualizar()
+	},[list.length])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="card container d-flex mt-3">
+  			<div className="card-body">
+  				<input type="text" className="input m-1 w-75" value={tarea} id="exampleInput" aria-describedby="inputHelp" onChange={(e)=>{setTarea(e.target.value)}} placeholder="No hay tareas, añadir tareas"/>
+				<button type="submit" className="btn btn-primary btn-sm" onChange={(e)=>{setList(e.target.value)}} onClick={enviarDatos}>Agregar</button>
+  			</div>
+			<div className="to-do-list d-flex">
+			  <ul>{list.map((item, index) => (
+        <li key={index}>
+          {item}
+          <button className="btn" onClick={() => DeleteItems(index)}>
+            <i className="fas fa-trash-alt" />
+          </button>
+        </li>
+      ))}</ul>
+			</div>
+			<button type="submit" className="btn btn-danger btn-sm" onClick={borrarLista}>Delete list</button>
 		</div>
+		
+		</>
 	);
 };
 
